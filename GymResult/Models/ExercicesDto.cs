a7f73f983e8c8f23ad29e43ead486e1ede1110ce
Patryk <Patryk@DESKTOP-Q6FMC5I>
2023-02-08 13:37:13 +0100

    Dodaj pliki projektów.

---
+++ GymResult/Models/ExercicesDto.cs
@@ -0,0 +1,10 @@
+﻿namespace GymResult.Models
+{
+    public class ExercicesDto
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public double Weight { get; set; }
+        public int Series { get; set; }
+    }
+}
---
+++ GymResult/Models/TrainingDto.cs
@@ -0,0 +1,16 @@
+﻿namespace GymResult.Models
+{
+    public class TrainingDto
+    {
+        public int Id { get; set; }
+        public DateTime Name { get; set; } = DateTime.Now;
+        public string Description { get; set; }
+
+        public string Location { get; set; }
+
+ 
+
+        public ICollection<ExercicesDto> exercies { get; set; } = new List<ExercicesDto>();
+
+    }
+}
